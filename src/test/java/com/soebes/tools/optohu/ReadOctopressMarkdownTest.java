56356d71dcc2f45f6016bd149aa86af69b8c9e0a
Karl Heinz Marbaise <kama@soebes.de>
2022-06-21 20:08:56 +0200

    WIP: continued.

--- src/test/java/com/soebes/tools/optohu/ReadOctopressMarkdownTest.java
+++ src/test/java/com/soebes/tools/optohu/ReadOctopressMarkdownTest.java
@@ -3,6 +3,7 @@ package com.soebes.tools.optohu;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -33,7 +34,17 @@ class ReadOctopressMarkdownTest {
 
   private Pattern TITLE = Pattern.compile("^title: \"(.*?)\"");
 
-  void convertToXX(List<String> lines) {
+  //date: 2019-01-21 19:45:57
+  private Pattern DATETIME = Pattern.compile("^date: (.*?)");
+
+  // categories: [Neuigkeiten,BM,Maven,Maven-Plugins,Maven-Plugin-Releases]
+  private Pattern CATEGORIES = Pattern.compile("categories: \\[(.*?)\\]");
+
+  record Post(Layout layout, String title, String dateTime, List<String> categories, List<String> content) {
+
+  }
+
+  Post convertToXX(List<String> lines) {
     // check minimum size (number of lines) 7
     if (!lines.get(0).equals("---")) {
       throw new IllegalStateException("Beginning of the post is not correct missing ---");
@@ -41,19 +52,49 @@ class ReadOctopressMarkdownTest {
     if (!lines.get(1).equals("layout: post")) {
       throw new IllegalStateException("layout: post Not found.");
     }
-    var matcher = TITLE.matcher(lines.get(2));
-    if (!matcher.matches()) {
+    var layout = Layout.post;
+    var titleLine = TITLE.matcher(lines.get(2));
+    if (!titleLine.matches()) {
       throw new IllegalStateException("Title not found");
     }
-    var title = matcher.group(0);
+    var title = titleLine.group(0);
 
+    var dateTimeLine = DATETIME.matcher(lines.get(3));
+    if (!dateTimeLine.matches()) {
+      throw new IllegalStateException("Date not found");
+    }
 
+    var dateTime = dateTimeLine.group(0);
+
+    if (!lines.get(4).equals("comments: true")) {
+      throw new IllegalStateException("comments not found.");
+    }
+
+    var categoriesLine = CATEGORIES.matcher(lines.get(5));
+    if (!categoriesLine.matches()) {
+      throw new IllegalStateException("Categories not found");
+    }
+
+    var categoriesStr = categoriesLine.group(0);
+    var categoriesArr = categoriesStr.split(",");
+    var categories = Arrays.stream(categoriesArr).map(String::trim).toList();
+
+    if (!lines.get(6).equals("---")) {
+      throw new IllegalStateException("End marker not found");
+    }
+
+    var postLines = new ArrayList<String>();
+    for (int i = 7; i < lines.size(); i++) {
+      postLines.add(lines.get(i));
+    }
+    
     /*
 s = date: 2019-01-21 19:45:57
 s = comments: true
 s = categories: [Neuigkeiten,BM,Maven,Maven-Plugins,Maven-Plugin-Releases]
 
      */
+    return new Post(layout, title, dateTime, categories, postLines);
   }
 
   @Test
