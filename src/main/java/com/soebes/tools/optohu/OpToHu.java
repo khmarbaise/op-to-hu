0cdc1f3e7ea86e330d85d2db0d4517914a7c32d4
Karl Heinz Marbaise <kama@soebes.de>
2022-06-22 21:30:45 +0200

    Improved.

--- src/main/java/com/soebes/tools/optohu/OpToHu.java
+++ src/main/java/com/soebes/tools/optohu/OpToHu.java
@@ -3,7 +3,6 @@ package com.soebes.tools.optohu;
 import static com.soebes.tools.optohu.Markdown.intoPost;
 import static java.lang.System.out;
 
-import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
@@ -15,10 +14,7 @@ class OpToHu {
     out.println("Op-To-Hu (Octopress to Hugo) Converter");
 
     var pathStream = DirectoryTool.readRecursively(Paths.get(args[0]));
-    var markdownFiles = pathStream.stream()
-        .filter(Files::isRegularFile)
-        .filter(s -> s.toString().endsWith(".md"))
-        .toList();
+    var markdownFiles = pathStream.stream().filter(s -> s.toString().endsWith(".md")).toList();
 
     var blogPosts = markdownFiles.stream().map(File::intoLines).map(intoPost).toList();
 
