5c9a268d9a6978801b58a7e0c5d23afd3e28201c
Karl Heinz Marbaise <kama@soebes.de>
2022-06-24 21:04:16 +0200

    Improved.

--- src/main/java/com/soebes/tools/optohu/Post.java
+++ src/main/java/com/soebes/tools/optohu/Post.java
@@ -6,12 +6,7 @@ import java.util.List;
 
 record Post(Path file, Layout layout, PostType postType, String title, String publishingTime, List<String> categories,
             List<String> content) {
-
-  static Post from(Post post) {
-    return new Post(post.file(), post.layout(), post.postType(), post.title(), post.publishingTime(), post.categories(),
-        post.content());
-  }
-
+  
   static Post from(Post post, List<String> content) {
     return new Post(post.file(), post.layout(), post.postType(), post.title(), post.publishingTime(), post.categories(),
         content);
