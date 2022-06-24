83f3d3c3ce5ea9d2a58124fd98bab9a68b04074b
Karl Heinz Marbaise <kama@soebes.de>
2022-06-24 20:14:40 +0200

    Improved.

--- src/main/java/com/soebes/tools/optohu/Highlighter.java
+++ src/main/java/com/soebes/tools/optohu/Highlighter.java
@@ -4,16 +4,14 @@ import java.util.function.UnaryOperator;
 
 public interface Highlighter {
 
-  /*
-{% highlight bash linenos %}
-svn mkdir URL/dir1 URL/dir2 URL/dir3 -m"- Create directory structure"
-{% endhighlight %}
-   */
-
   UnaryOperator<Post> migrate = post -> {
-    var content = post.content().stream().map(ApacheJiraLink.interpolate).toList();
-    return new Post(post.file(), post.layout(), post.postType(), post.title(), post.publishingTime(), post.categories(),
-        content);
+    var content = post.content().stream().map(line -> switch (line) {
+      case "{% highlight bash linenos %}" -> "```bash";
+      case "{% endhighlight %}" -> "```";
+      default -> line;
+    }).toList();
+
+    return Post.from(post, content);
   };
 
 }
---
+++ src/test/java/com/soebes/tools/optohu/HighlighterTest.java
@@ -0,0 +1,34 @@
+package com.soebes.tools.optohu;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class HighlighterTest {
+
+  @Test
+  void emptyContent() {
+    var mockPost = mock(Post.class);
+    var post = Post.from(mockPost, List.of());
+    var result = Highlighter.migrate.apply(post);
+    assertThat(result.content()).isEmpty();
+  }
+
+  @Test
+  void singleLineContent() {
+    var mockPost = mock(Post.class);
+    var post = Post.from(mockPost, List.of("Test Line"));
+    var result = Highlighter.migrate.apply(post);
+    assertThat(result.content()).containsExactly("Test Line");
+  }
+
+  @Test
+  void startingHighlight() {
+    var mockPost = mock(Post.class);
+    var post = Post.from(mockPost, List.of("{% highlight bash linenos %}", "Highlighted", "{% endhighlight %}"));
+    var result = Highlighter.migrate.apply(post);
+    assertThat(result.content()).containsExactly("```bash", "Highlighted", "```");
+  }
+}
