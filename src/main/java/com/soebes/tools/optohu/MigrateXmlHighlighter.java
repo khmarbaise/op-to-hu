bb659c73661a13a3dc051efddad9c4732f0326f7
Karl Heinz Marbaise <kama@soebes.de>
2023-02-24 22:44:54 +0100

    Improved code.

--- src/main/java/com/soebes/tools/optohu/MigrateXmlHighlighter.java
+++ src/main/java/com/soebes/tools/optohu/MigrateXmlHighlighter.java
@@ -2,9 +2,9 @@ package com.soebes.tools.optohu;
 
 import java.util.function.UnaryOperator;
 
-abstract class MigrateXmlHighlighter {
+interface MigrateXmlHighlighter {
 
-  public static final UnaryOperator<Post> migrateContent = post -> {
+  UnaryOperator<Post> migrateContent = post -> {
     var content = post.content();
     var contentLines = content.lines().stream().map(line -> switch (line) {
       case "``` xml", "```xml" -> "```xml";
